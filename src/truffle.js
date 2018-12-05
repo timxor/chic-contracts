f7354a96414e1aca7a32609a9ac3b49cd75a4d82
Tim Siwula <tcsiwula@gmail.com>
2018-12-04 19:25:47 -0600

    deployed JO ERC-20 token contract

--- src/truffle.js
+++ src/truffle.js
@@ -1,33 +1,42 @@
-/*
-* NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
-* function when declaring them. Failure to do so will cause commands to hang. ex:
-* ```
-* mainnet: {
-*     provider: function() {
-*       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3/36e8ae2157cb4b19af9dfc36dfb71539')
-*     },
-*     network_id: '1',
-*     gas: 4500000,
-*     gasPrice: 10000000000,
-*   },
-*/
+require('dotenv').config();  // Store environment-specific variable from '.env' to process.env
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const url = process.env.INFURA_API_KEY;
 
 module.exports = {
-    networks: {
-        infura: {
-            provider: function () {
-                return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3/36e8ae2157cb4b19af9dfc36dfb71539')
-            },
-            network_id: '1',
-            gas: 4500000,
-            gasPrice: 10000000000,
-        },
-        kovan: {
-            host: "127.0.0.1",
-            port: 8545,
-            network_id: 42,
-            gas: 4700000,
-            gasPrice: 25000000000
-        },
-    }
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 21
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 21
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 6712390
+    },
+    localRinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 4
+    },
+    main: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + url),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 21
+    },
+  }
 };
